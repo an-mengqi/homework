0523e1b1855b07f5aecd98375131eb7163ed1f3c
an-mengqi <anklopova@gmail.com>
2022-11-19 13:05:15 +0300

    docstrings for tests in service3 added

--- hw4_api_testing/tests/test_service3.py
+++ hw4_api_testing/tests/test_service3.py
@@ -6,12 +6,14 @@ from jsonschema import validate
 
 
 def test_check_header(service3_jsonplaceholder_url):
+    """Check the value of 'Server' parameter in headers"""
     response = requests.get(service3_jsonplaceholder_url + "posts")
     assert response.headers["Server"] == "cloudflare"
 
 
 @pytest.mark.parametrize("number", ["1", "2", "3", "4", "5"])
 def test_validate_json_posts(service3_jsonplaceholder_url, number):
+    """Validate json in response with jsonschema"""
     response = requests.get(service3_jsonplaceholder_url + "posts/" + number)
     response_json = response.json()
 
@@ -31,6 +33,7 @@ def test_validate_json_posts(service3_jsonplaceholder_url, number):
 
 @pytest.mark.parametrize("number", ["1", "2", "3", "4", "5"])
 def test_validate_json_comments(service3_jsonplaceholder_url, number):
+    """Validate json in response with Cerberus"""
     response = requests.get(service3_jsonplaceholder_url + "comments/" + number)
     response_json = response.json()
 
@@ -48,6 +51,7 @@ def test_validate_json_comments(service3_jsonplaceholder_url, number):
 
 @pytest.mark.xfail(reason="server will not be really update posts number")
 def test_delete_post(service3_jsonplaceholder_url):
+    """Check the opportunity to delete a post"""
     response = requests.delete(service3_jsonplaceholder_url + "posts/1")
     all = requests.get(service3_jsonplaceholder_url + "posts")
     assert response.status_code == 200
@@ -55,5 +59,6 @@ def test_delete_post(service3_jsonplaceholder_url):
 
 
 def test_check_photos_number(service3_jsonplaceholder_url):
+    """Check the length of elements in json response"""
     response = requests.get(service3_jsonplaceholder_url + "photos")
     assert len(response.json()) == 5000
