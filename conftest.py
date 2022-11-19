2998cd9c26b5b8010455c781d4d8059c33329ab3
an-mengqi <anklopova@gmail.com>
2022-11-19 13:19:31 +0300

    test with arguments for service4 added

--- conftest.py
+++ conftest.py
@@ -23,3 +23,18 @@ def service2_brewery_url():
 def service3_jsonplaceholder_url():
     jsonplaceholder_url = "https://jsonplaceholder.typicode.com/"
     return jsonplaceholder_url
+
+
+def pytest_addoption(parser):
+    parser.addoption("--url", default="https://ya.ru")
+    parser.addoption("--status_code", default="200")
+
+
+@pytest.fixture
+def url(request):
+    return request.config.getoption("--url")
+
+
+@pytest.fixture
+def status_code(request):
+    return request.config.getoption("--status_code")
